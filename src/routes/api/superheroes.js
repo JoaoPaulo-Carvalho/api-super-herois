2560854765d4c453643ae9dea3723542258b52a1
João Paulo de Carvalho Araújo <araujo.jpc@outlook.com>
2021-04-09 00:14:50 -0300

    feature: refatoração código, removendo require de biblioteca removida

--- src/routes/api/superheroes.js
+++ src/routes/api/superheroes.js
@@ -2,7 +2,6 @@ const express = require('express')
 const fs = require('fs/promises')
 const path = require('path')
 const axios = require('axios')
-const _ = require('lodash')
 
 const router = express.Router()
 
